f1b40bb4cefa19662aab788b0b6f200ca02a699e
daud <duadarts@gmail.com>
2022-07-23 09:01:45 +0300

    authentication

--- config/mail.php
+++ config/mail.php
@@ -37,12 +37,12 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_HOST','smtp.googlemail.com
+            'local_domain' => env('MAIL_HOST','smtp.gmail.com
 '),
         ],
 
